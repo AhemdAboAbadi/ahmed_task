0903180c0357e60b85c62603745c07c92b233630
AhemdAboAbadi <ahmed.m.aboabadi@gmail.com>
2023-05-19 16:22:20 +0300

    add apollo and MUI theme

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { ApolloProvider } from "@apollo/client";
+import { CssBaseline, ThemeProvider } from "@material-ui/core";
+import { createTheme } from "@material-ui/core/styles";
+import client from "./services/apollo";
+import { HomeScreen } from "./pages";
+
+const theme = createTheme();
+
+const App = () => {
+  return (
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <HomeScreen />
+      </ThemeProvider>
+    </ApolloProvider>
+  );
+};
+
+export default App;
